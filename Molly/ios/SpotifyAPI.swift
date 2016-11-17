3953ab2ecb40a4d6646da018ce1968b47102b1bc
Rachit Kataria <rachitka@usc.edu>
2016-11-17 02:32:24 -0800

    Added setIsPlaying and getCurrentSeconds

--- Molly/ios/SpotifyAPI.swift
+++ Molly/ios/SpotifyAPI.swift
@@ -27,4 +27,45 @@ class SpotifyAPI: NSObject {
     self.auth.redirectURL = URL(string: redirectURL);
     self.auth.requestedScopes = [SPTAuthStreamingScope];
   }
+  
+  @objc(playing:callback:)
+  func setIsPlaying(playing:Bool, callback:@escaping RCTResponseSenderBlock) {
+    self.player.setIsPlaying(playing) { (error) -> Void in
+      
+      if error == nil {
+        // Pause
+        callback([NSNull(), NSNull()])
+        
+      } else {
+        print("Failed to stop spotify player: " + (error?.localizedDescription)!)
+        callback([error!, NSNull()])
+        
+        self.checkSession()
+      }
+    }
+  }
+  
+  
+  @objc(callback:)
+  func getCurrentSeconds(callback:RCTResponseSenderBlock) {
+     callback([NSNull(), self.player.playbackState.position])
+  }
+  
+  //Check if session is valid and renew it if not
+  func checkSession() {
+    if(self.auth.session.isValid()) {
+      self.auth.renewSession(auth.session, callback: { (error, session) in
+        if(error != nil) {
+          print(error!)
+//          [sharedManager startAuth:sharedManager.clientID setRedirectURL:sharedManager.myScheme setRequestedScopes:sharedManager.requestedScopes];
+//        } else {
+//          [sharedManager setSession:session];
+//          [[sharedManager player] loginWithAccessToken:session.accessToken];
+//        }
+//      }];
+
+        }
+      })
+    }
+  }
 }
