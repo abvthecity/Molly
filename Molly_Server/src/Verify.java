aaf6842f707e427a5986a9e2b9483ba181d2ce73
Shivangi1296 <goelshiv@usc.edu>
2016-11-17 13:40:09 -0800

    servlet

--- Molly_Server/src/Verify.java
+++ Molly_Server/src/Verify.java
@@ -25,9 +25,20 @@ public class Verify extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String info = request.getParameter("hi");
+		
+//		response.setHeader();
+		response.getWriter().append("{\"object\": \"key\"}");
 	}
 
 	/**
