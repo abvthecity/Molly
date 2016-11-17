b3bf46fb9ae883de0949ac906356635db0eb8219
Aniruddh Bharadwaj <aniruddh@usc.edu>
2016-11-17 02:54:00 -0800

    api helper updates

--- Molly/ios/SpotifyAPI.swift
+++ Molly/ios/SpotifyAPI.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import SafariServices
 
+@available(iOS 9.0, *)
 @objc(SpotifyAPI)
-class SpotifyAPI: NSObject {
+class SpotifyAPI: NSObject, SPTAudioStreamingDelegate {
   
   var player: SPTAudioStreamingController!
   
   /* SPOTIFY AUTH SECTION */
   var auth: SPTAuth!
+  var authVC: SFSafariViewController!
   
-  @objc(authenticate:clientID:redirectURL:callback:)
+  @objc(authenticate:redirectURL:callback:)
   func authenticate(clientID: String!, redirectURL: String!, callback: RCTResponseSenderBlock) {
     // set auth and player
     self.auth = SPTAuth.defaultInstance();
@@ -26,5 +29,34 @@ class SpotifyAPI: NSObject {
     self.auth.clientID = clientID;
     self.auth.redirectURL = URL(string: redirectURL);
     self.auth.requestedScopes = [SPTAuthStreamingScope];
+    
+    // become the delegate to the streaming controller
+    self.player.delegate = self
+    
+    // start player
+    do {
+      try self.player.start(withClientId: self.auth.clientID)
+    } catch {
+      // do nothing!
+    }
+    
+    // start auth on main thread
+    DispatchQueue.main.async {
+      self.startUserAuth()
+    }
+  }
+  
+  // auth helper
+  func startUserAuth() {
+    // if sesh is valid, login with existing token
+    if (self.auth.session.isValid()) {
+      self.player.login(withAccessToken: self.auth.session.accessToken)
+    } else {
+      // construct login url and open in sfvc
+      var loginURL: URL = self.auth.spotifyWebAuthenticationURL()
+      self.authVC = SFSafariViewController.init(url: loginURL)
+      let delegate = UIApplication.shared.delegate as! AppDelegate
+      delegate.window.rootViewController?.present(self.authVC, animated: true, completion: nil);
+    }
   }
 }
