dab3f78681f119010e0ab0ce9645014b5e5ab480
Lernik Tahmasian <lernikcs104@users.noreply.github.com>
2016-11-17 17:27:17 -0800

    Add files via upload
    channel and User classes

--- Molly_Server/src/classes/Channel.java
+++ Molly_Server/src/classes/Channel.java
@@ -1,5 +1,62 @@
-package classes;
+package sql;
 
 public class Channel {
 
+	public int channelID;
+	public String clientID;
+	public String[] channelTags;
+	public int numChannelSubscribers;
+	public int numChannelLikes; 
+	
+	
+	public channel(int channelId, String clientId, String[] channelTags, int numSubscribers, int numlikes ){
+		this.channelID = channelId;
+		this.clientID = clientId;
+		this.channelTags = channelTags;
+		this.numChannelSubscribers = numSubscribers;
+		this.numChannelLikes = numlikes;
+		
+	}
+	
+	public int getChannelID(){
+		return channelID;
+		
+	}
+	public String getClientID(){
+		return clientID;
+		
+	}
+	public String[] getChannelTags(){
+		return channelTags;
+		
+	}
+	public int getNumSubscribers(){
+		return numChannelSubscribers;
+		
+	}
+	public int getNumLikes(){
+		return numChannelLikes;
+		
+	}
+	
+	public void setChannelID(int chId){
+		 channelID = chId;
+		
+	}
+	public void getClientID(String clientId){
+		return clientID = clientId;
+		
+	}
+	public  addChannelTags(int pos, String[]  tags){
+		 channelTags[pos]=tags;
+		
+	}
+	public void setNumSubscribers(int numSubscribes){
+		 numChannelSubscribers=numSubscribes;
+		
+	}
+	public void setNumLikes(int numLikes){
+		 numChannelLikes = numLikes;
+		
+	}
 }
