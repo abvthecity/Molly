24dc42eac21cefe9418a29c48e249ac84b03f442
Shivangi1296 <goelshiv@usc.edu>
2016-11-19 06:04:15 -0800

    updatePlaylistMessageEncoder

--- Molly_Server/src/encoders/UpdatePlaylistMessageEncoder.java
+++ Molly_Server/src/encoders/UpdatePlaylistMessageEncoder.java
@@ -1,5 +1,29 @@
 package encoders;
 
-public class UpdatePlaylistMessageEncoder {
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+import messages.UpdatePlaylistMessage;
+
+public class UpdatePlaylistMessageEncoder implements Encoder.Text<UpdatePlaylistMessage>{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(EndpointConfig arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String encode(UpdatePlaylistMessage arg0) throws EncodeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
