276efed9c1d18e432519eaa3faa8c05ad1bf41cf
Shivangi1296 <goelshiv@usc.edu>
2016-11-19 18:33:44 -0800

    join channel message

--- Molly_Server/src/messages/JoinChannelMessage.java
+++ Molly_Server/src/messages/JoinChannelMessage.java
@@ -1,5 +1,26 @@
 package messages;
 
-public class JoinChannelMessage {
+public class JoinChannelMessage extends Message {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1375336583938391483L;
+	private String channelID;
+	
+	public String getChannelID() {
+		return channelID;
+	}
 
+	public void setChannelID(String channelID) {
+		this.channelID = channelID;
+	}
+
+	public JoinChannelMessage(String clientID, String isDJ, String isLive, String currDJ, String channelID) {
+		// TODO Auto-generated constructor stub
+		this.clientID = clientID;
+		this.isDJ = isDJ;
+		this.isLive = isLive;
+		this.currDJ = currDJ;
+		this.channelID = channelID;
+	}
 }
