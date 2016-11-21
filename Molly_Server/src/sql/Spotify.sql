ba0ace1ff9f6ed106c2518c06f96ac6e576d7912
Lernik Tahmasian <ltahmasi@usc.edu>
2016-11-20 16:34:26 -0800

    updated SPOTIFT.sql

--- Molly_Server/src/sql/Spotify.sql
+++ Molly_Server/src/sql/Spotify.sql
@@ -3,24 +3,23 @@ CREATE DATABASE SpotifyDJ;
 use SpotifyDJ;
 
 CREATE TABLE `SpotifyDJ`.`Users` (
-  `clientID` VARCHAR(10) NOT NULL,
-  `clientTags` VARCHAR(1000) NOT NULL,
-  `clientDJ` bool NULL,
-  `clientBookmarks` VARCHAR(1000) NULL,
-  PRIMARY KEY (`clientID`));
-  
-  CREATE TABLE `SpotifyDJ`.`Channel` (
-  `clientID` VARCHAR(20) NOT NULL,
-  `channelTags` Varchar(1000) Null,
-  `subscribersNum` int NULL,
-  `likesNum` int NULL,
-  PRIMARY KEY (`clientID`));
-  
+`clientID` VARCHAR(10) NOT NULL,
+`clientTags` VARCHAR(1000) NOT NULL,
+`clientDJ` bool NULL,
+`clientBookmarks` VARCHAR(1000) NULL,
+PRIMARY KEY (`clientID`)
+);
+
+CREATE TABLE `SpotifyDJ`.`Channel` (
+`clientID` VARCHAR(20) NOT NULL,
+`channelTags` Varchar(1000) Null,
+`subscribersNum` int NULL,
+`likesNum` int NULL,
+PRIMARY KEY (`clientID`)
+);
+
 CREATE TABLE `SpotifyDJ`.`channelTags` (
-  `channelTag` VARCHAR(100) NOT NULL,
-  `channelClientID` VARCHAR(1000) NULL,
-  PRIMARY KEY (`channelTag`));
-
-
-
---   `songURIPlaylist` BLOB NULL
+`channelTag` VARCHAR(100) NOT NULL,
+`channelClientID` VARCHAR(1000) NULL,
+PRIMARY KEY (`channelTag`)
+);
